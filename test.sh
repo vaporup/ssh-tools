b18fc2b583451786916435344fe655fb8fffc2e7
Sven Wick <sven.wick@gmx.de>
2020-01-22 18:06:20 +0100

    Remove spaces

--- test.sh
+++ test.sh
@@ -8,21 +8,21 @@ TEST_SSH_SERVER="192.168.1.10"
 SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
 
 
- test_commands=( "${SCRIPT_PATH}/ssh-keyinfo ${SCRIPT_PATH}/examples/*/*"                               )
-test_commands+=( "${SCRIPT_PATH}/ssh-certinfo ${SCRIPT_PATH}/examples/*/*"                              )
-test_commands+=( "${SCRIPT_PATH}/ssh-certinfo -v ${SCRIPT_PATH}/examples/*/*"                           )
-test_commands+=( "${SCRIPT_PATH}/ssh-certinfo -c ${SCRIPT_PATH}/examples/*/*"                           )
-test_commands+=( "${SCRIPT_PATH}/ssh-certinfo -cv ${SCRIPT_PATH}/examples/*/*"                          )
-test_commands+=( "${SCRIPT_PATH}/ssh-certinfo -c -w 20000 ${SCRIPT_PATH}/examples/*/*"                  )
-test_commands+=( "${SCRIPT_PATH}/ssh-certinfo -cv -w 20000 ${SCRIPT_PATH}/examples/*/*"                 )
-test_commands+=( "CHECK_REMOTE_FILE_EXISTS=NO sshpass -e ${SCRIPT_PATH}/ssh-diff /etc/hosts ${TEST_SSH_USER}@${TEST_SSH_SERVER}"    )
+ test_commands=( "${SCRIPT_PATH}/ssh-keyinfo ${SCRIPT_PATH}/examples/*/*" )
+test_commands+=( "${SCRIPT_PATH}/ssh-certinfo ${SCRIPT_PATH}/examples/*/*" )
+test_commands+=( "${SCRIPT_PATH}/ssh-certinfo -v ${SCRIPT_PATH}/examples/*/*" )
+test_commands+=( "${SCRIPT_PATH}/ssh-certinfo -c ${SCRIPT_PATH}/examples/*/*" )
+test_commands+=( "${SCRIPT_PATH}/ssh-certinfo -cv ${SCRIPT_PATH}/examples/*/*" )
+test_commands+=( "${SCRIPT_PATH}/ssh-certinfo -c -w 20000 ${SCRIPT_PATH}/examples/*/*" )
+test_commands+=( "${SCRIPT_PATH}/ssh-certinfo -cv -w 20000 ${SCRIPT_PATH}/examples/*/*" )
+test_commands+=( "CHECK_REMOTE_FILE_EXISTS=NO sshpass -e ${SCRIPT_PATH}/ssh-diff /etc/hosts ${TEST_SSH_USER}@${TEST_SSH_SERVER}" )
 test_commands+=( "CHECK_REMOTE_FILE_EXISTS=NO sshpass -e ${SCRIPT_PATH}/ssh-diff -y /etc/hosts ${TEST_SSH_USER}@${TEST_SSH_SERVER}" )
 test_commands+=( "CHECK_REMOTE_FILE_EXISTS=NO sshpass -e ${SCRIPT_PATH}/ssh-diff -u /etc/hosts ${TEST_SSH_USER}@${TEST_SSH_SERVER}" )
-test_commands+=( "sshpass -e ${SCRIPT_PATH}/ssh-facts ${TEST_SSH_USER}@${TEST_SSH_SERVER}"              )
-test_commands+=( "${SCRIPT_PATH}/ssh-hostkeys ${TEST_SSH_SERVER}"                                       )
-test_commands+=( "sshpass -e ${SCRIPT_PATH}/ssh-ping -v -c 3 -D ${TEST_SSH_USER}@${TEST_SSH_SERVER}"    )
-test_commands+=( "sshpass -e ${SCRIPT_PATH}/ssh-ping -v -c 3 -H ${TEST_SSH_USER}@${TEST_SSH_SERVER}"    )
-test_commands+=( "${SCRIPT_PATH}/ssh-version ${TEST_SSH_SERVER}"                                        )
+test_commands+=( "sshpass -e ${SCRIPT_PATH}/ssh-facts ${TEST_SSH_USER}@${TEST_SSH_SERVER}" )
+test_commands+=( "${SCRIPT_PATH}/ssh-hostkeys ${TEST_SSH_SERVER}" )
+test_commands+=( "sshpass -e ${SCRIPT_PATH}/ssh-ping -v -c 3 -D ${TEST_SSH_USER}@${TEST_SSH_SERVER}" )
+test_commands+=( "sshpass -e ${SCRIPT_PATH}/ssh-ping -v -c 3 -H ${TEST_SSH_USER}@${TEST_SSH_SERVER}" )
+test_commands+=( "${SCRIPT_PATH}/ssh-version ${TEST_SSH_SERVER}" )
 
 for (( i = 0; i < ${#test_commands[@]} ; i++ )); do
   printf "\n**** Running: ${test_commands[$i]} *****\n\n"
