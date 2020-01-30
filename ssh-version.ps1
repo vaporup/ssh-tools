9b9d816b94b2d01abf6705dc26edebf3160cfae3
Sven Wick <sven.wick@gmx.de>
2020-01-30 17:10:12 +0100

    pwsh highlights found string. Get complete line

--- ssh-version.ps1
+++ ssh-version.ps1
@@ -1,2 +1,2 @@
-$output = ( ssh -vN -o BatchMode=yes $args -l ForceLogin2Fail 2>&1 ) | Select-String "remote software version" | Out-String
+$output = ( ssh -vN -o BatchMode=yes $args -l ForceLogin2Fail 2>&1 ) | Select-String ".*remote software version.*" | Out-String
 $output.Replace("debug1: ","")
