3005384a5af17b7b9356b188bee6ba8be87ab7be
Sven Wick <sven.wick@gmx.de>
2020-01-31 01:31:00 +0100

    Add Help

--- ssh-version.ps1
+++ ssh-version.ps1
@@ -1,2 +1,21 @@
+
+<#
+
+.SYNOPSIS
+Shows version of the SSH server you are connecting to
+
+.Description
+Connects to a server and detects which version of SSH is running
+
+.EXAMPLE
+./ssh-version.ps1 192.168.1.10
+
+#>
+
+if($args.Count -eq 0) {
+    Get-Help $MyInvocation.MyCommand.Definition
+    return
+}
+
 $output = ( ssh -vN -o BatchMode=yes $args -l ForceLogin2Fail 2>&1 ) | Select-String ".*remote software version.*" | Out-String
 $output.Replace("debug1: ","")
